d52b61282b950d31a2371b5676d628d0556e492a
Hanson-HSChang <hanson.hschang@gmail.com>
2024-08-19 22:36:33 -0500

    wip: update listener

--- ros2-vicon/multi_topic_listener.py
+++ ros2-vicon/multi_topic_listener.py
@@ -17,6 +17,7 @@ from std_msgs.msg import String as StdString  # For publishing test_output
 from std_msgs.msg import Float32MultiArray, MultiArrayDimension  # For publishing numpy array
 
 import numpy as np
+from time import perf_counter
 
 class MultiSubscriberNode(Node):
     def __init__(self):
@@ -32,7 +33,7 @@ class MultiSubscriberNode(Node):
         # Subscribe to the first topic
         self.subscription_1 = self.create_subscription(
             Position,
-            '/vicon_mock/TestSubject_0/CrossSection_0_0',
+            '/vicon/TestSubject_0/TestSubject_0',
             self.listener_callback_1,
             1
         )
@@ -41,7 +42,7 @@ class MultiSubscriberNode(Node):
         # Subscribe to the second topic
         self.subscription_2 = self.create_subscription(
             Position,
-            '/vicon_mock/TestSubject_0/CrossSection_0_1',
+            '/vicon/TestSubject_1/TestSubject_1',
             self.listener_callback_2,
             1
         )
@@ -50,23 +51,32 @@ class MultiSubscriberNode(Node):
         # Store received messages (optional)
         self.data_1 = None
         self.data_2 = None
+        self.data_1_frame_number = None
+        self.data_2_frame_number = None
+        self.start_time = perf_counter()
 
     def listener_callback_1(self, msg):
-        self.data_1 = msg.x_trans
+        self.data_1 = [msg.x_trans, msg.y_trans, msg.z_trans]
+        self.data_1_frame_number = msg.frame_number
         self.get_logger().info(f'Received message from topic_1: {self.data_1}')
         self.process_and_publish()
 
     def listener_callback_2(self, msg):
-        self.data_2 = msg.x_trans
+        self.data_2 = [msg.x_trans, msg.y_trans, msg.z_trans]
+        self.data_2_frame_number = msg.frame_number
         self.get_logger().info(f'Received message from topic_2: {self.data_2}')
         self.process_and_publish()
 
     def process_and_publish(self):
         if self.data_1 and self.data_2:
+            # Calculate time elapsed since the first message was received
+            elapsed_time = perf_counter() - self.start_time
+            self.get_logger().info(f'sampling rate: {1/elapsed_time} Hz')
+
             # Create a 2D NumPy array
             np_array = np.array([
-                [self.data_1, self.data_2],
-                [self.data_1, self.data_2],
+                self.data_1,
+                self.data_2,
             ])
 
             # Create Float32MultiArray message
@@ -84,12 +94,14 @@ class MultiSubscriberNode(Node):
 
             # Publish the processed data
             self.numpy_publisher.publish(msg)
-            self.get_logger().info(f'Published 2D NumPy array with shape {np_array}')
+            self.get_logger().info(f'Published 2D NumPy array with {self.data_1_frame_number} and {self.data_2_frame_number}')
+            self.get_logger().info(f'{np_array}')
 
             # Optionally, reset data to avoid re-publishing the same data
             self.data_1 = None
             self.data_2 = None
-            time.sleep(1)
+            # time.sleep(1)
+        self.start_time = perf_counter()
 
 def main(args=None):
     rclpy.init(args=args)
