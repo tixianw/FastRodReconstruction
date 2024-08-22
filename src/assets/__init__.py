fe88d7c5328efca0ebe5977464e9878d89c515fe
Hanson-HSChang <hanson.hschang@gmail.com>
2024-08-22 13:07:43 -0500

    feat: add required data files in assets

--- src/assets/__init__.py
+++ src/assets/__init__.py
@@ -1,3 +1,3 @@
 ASSETS = "assets"
-FILE_NAME = "BR2_arm_data.npy"
-MODEL_NAME = "data_smoothing_model_br2_6markers.pt"
+FILE_NAME = "BR2_arm_data_4markers.npy"
+MODEL_NAME = "data_smoothing_model_br2_4markers.pt"
