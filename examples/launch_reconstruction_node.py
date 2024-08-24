wip: fix #10 zero quaternion issue, and refactor

examples/launch_reconstruction_node.py
@@ -1,13 +1,13 @@
-from typing import List, Optional, Tuple
+from typing import Dict, List, Optional, Tuple, Union
 
-from collections import defaultdict
+from dataclasses import dataclass
 
 import click
 import numpy as np
 
 from neural_data_smoothing3D import pos_dir_to_input
 from reconstruction import ReconstructionModel, ReconstructionResult
-from ros2_vicon import NDArrayPublisher, PoseSubscriber
+from ros2_vicon import NDArrayPublisher, PosePublisher, PoseSubscriber
 
 try:
     import rclpy
@@ -21,9 +21,10 @@ except ModuleNotFoundError:
     sys.exit(1)
 
 
-class ReconstructionModelResourceProtocol:
-    data_file_name: Optional[str] = (None,)
-    model_file_name: Optional[str] = (None,)
+@dataclass
+class ReconstructionModelResource:
+    data_file_name: Optional[str] = None
+    model_file_name: Optional[str] = None
 
 
 class ReconstructionNode(Node):
@@ -31,7 +32,7 @@ class ReconstructionNode(Node):
         self,
         subscription_topics: Tuple[str],
         reconstruction_rate: float = 60.0,
-        model_resource: Optional[ReconstructionModelResourceProtocol] = None,
+        model_resource: Optional[ReconstructionModelResource] = None,
         log_level: str = "info",
     ):
         super().__init__("reconstruction_node")
@@ -52,12 +53,11 @@ class ReconstructionNode(Node):
                 node=self,
             )
             self.__subscribers.append(subscriber)
-
-        self.number_of_markers = len(self.__subscribers)
+        self.init_input_data()
 
         # Initialize reconstruction model
         self.get_logger().info("- Reconstruction model initializing...")
-        self.__reconstruction_model = (
+        self.__model = (
             ReconstructionModel(
                 data_file_name=model_resource.data_file_name,
                 model_file_name=model_resource.model_file_name,
@@ -68,29 +68,35 @@ class ReconstructionNode(Node):
 
         # Initialize publishers
         self.get_logger().info("- Publishers initializing...")
-        self.__publishers = defaultdict(lambda: "No publisher")
-        self.__publishers["position"] = NDArrayPublisher(
-            topic="/reconstruction/position",
-            shape=self.__reconstruction_model.result.position.shape,
-            axis_labels=("position", "element"),
-            qos_profile=100,
-            node=self,
-        )
-        self.__publishers["directors"] = NDArrayPublisher(
-            topic="/reconstruction/directors",
-            shape=self.__reconstruction_model.result.directors.shape,
-            axis_labels=("directors", "director_index", "element"),
-            qos_profile=100,
-            node=self,
-        )
-
-        self.__publishers["kappa"] = NDArrayPublisher(
-            topic="/reconstruction/kappa",
-            shape=self.__reconstruction_model.result.kappa.shape,
-            axis_labels=("kappa", "element"),
-            qos_profile=100,
-            node=self,
-        )
+        self.__publishers: Dict[str, Union[PosePublisher, NDArrayPublisher]] = {
+            "pose": PosePublisher(
+                topic="/vicon/pose",
+                length=self.number_of_markers - 1,
+                qos_profile=100,
+                node=self,
+            ),
+            "position": NDArrayPublisher(
+                topic="/reconstruction/position",
+                shape=self.__model.result.position.shape,
+                axis_labels=("position", "element"),
+                qos_profile=100,
+                node=self,
+            ),
+            "directors": NDArrayPublisher(
+                topic="/reconstruction/directors",
+                shape=self.__model.result.directors.shape,
+                axis_labels=("directors", "director_index", "element"),
+                qos_profile=100,
+                node=self,
+            ),
+            "kappa": NDArrayPublisher(
+                topic="/reconstruction/kappa",
+                shape=self.__model.result.kappa.shape,
+                axis_labels=("kappa", "element"),
+                qos_profile=100,
+                node=self,
+            ),
+        }
 
         # Create a timer for publishing at reconstruction_rate Hz
         self.__timer = self.create_timer(
@@ -98,7 +104,13 @@ class ReconstructionNode(Node):
             callback=self.timer_callback,
         )
 
-    def set_logger_level(self, log_level: str):
+    def init_input_data(self) -> None:
+        self.number_of_markers = len(self.__subscribers)
+        self.__input_data = np.zeros((1, 4, 4, self.number_of_markers - 1))
+        self.__input_data[0, 3, 3, :] = 1.0
+        self.__input_data[0, :3, :3, :] = np.eye(3)
+
+    def set_logger_level(self, log_level: str) -> None:
         level_map = {
             "debug": rclpy.logging.LoggingSeverity.DEBUG,
             "info": rclpy.logging.LoggingSeverity.INFO,
@@ -114,82 +126,98 @@ class ReconstructionNode(Node):
 
     @property
     def result(self) -> ReconstructionResult:
-        return self.__reconstruction_model.result
+        return self.__model.result
 
-    def subscriber_callback_closure(self, i: int):
+    def subscriber_callback_closure(self, i: int) -> callable:
         def subscriber_callback(msg):
-            self.__subscribers[i].read(msg)
-            self.get_logger().info(f"{self.__subscribers[i]}")
+            self.__subscribers[i].receive(msg)
+            self.get_logger().debug(f"{self.__subscribers[i]}")
 
-            # self.get_logger().info(f'{msg.frame_number}')
-            # self.get_logger().info(f'  {msg.x_trans}')
-            # self.get_logger().info(f'  {msg.y_trans}')
-            # self.get_logger().info(f'  {msg.z_trans}')
-            # self.get_logger().info(f'  {msg.x_rot}')
-            # self.get_logger().info(f'  {msg.y_rot}')
-            # self.get_logger().info(f'  {msg.z_rot}')
-            # self.get_logger().info(f'  {msg.w}')
+            # self.get_logger().debug(f'{msg.frame_number}')
+            # self.get_logger().debug(f'  {msg.x_trans}')
+            # self.get_logger().debug(f'  {msg.y_trans}')
+            # self.get_logger().debug(f'  {msg.z_trans}')
+            # self.get_logger().debug(f'  {msg.x_rot}')
+            # self.get_logger().debug(f'  {msg.y_rot}')
+            # self.get_logger().debug(f'  {msg.z_rot}')
+            # self.get_logger().debug(f'  {msg.w}')
 
         return subscriber_callback
 
-    def timer_callback(self):
+    def timer_callback(self) -> None:
         self.reconstruct()
+        self.publish("pose", self.__input_data[0])
         self.publish("position", self.result.position)
         self.publish("directors", self.result.directors)
         self.publish("kappa", self.result.kappa)
 
     def create_input_data(self) -> np.ndarray:
         # Create input data from the subscribers
-
-        position_data = np.zeros((1, 3, self.number_of_markers - 1))
-        director_data = np.zeros((1, 3, 3, self.number_of_markers - 1))
         base_position = self.__subscribers[0].message.position
         base_directors = self.__subscribers[0].message.directors
         for i, subscriber in enumerate(self.__subscribers[1:]):
-            position_data[0, :, i] = subscriber.message.position - base_position
-            director_data[0, :, :, i] = subscriber.message.directors
+            self.__input_data[0, :3, 3, i] = (
+                subscriber.message.position - base_position
+            )
+            self.__input_data[0, :3, :3, i] = subscriber.message.directors
 
         input_data = pos_dir_to_input(
-            pos=position_data,
-            dir=director_data,
+            pos=self.__input_data[:, :3, 3, :],
+            dir=self.__input_data[:, :3, :3, :],
         )
         return input_data
 
     def reconstruct(self):
-        self.__reconstruction_model(self.create_input_data())
+        self.__model(self.create_input_data())
 
-    def publish(self, publisher_key: str, data: np.ndarray):
-        self.__publishers[publisher_key].publish(data)
+    def publish(self, publisher_key: str, data: np.ndarray) -> None:
+        self.__publishers[publisher_key].release(data)
         self.get_logger().info(f"{self.__publishers[publisher_key]}")
+
+
+def set_subsciption_topics(source: str):
+    if source.lower() == "vicon":
+        subscription_topics = (
+            "/vicon/br2_seg_1/br2_seg_1",
+            "/vicon/br2_seg_2/br2_seg_2",
+            "/vicon/br2_seg_3/br2_seg_3",
+            "/vicon/br2_seg_4/br2_seg_4",
+        )
+    if source.lower() == "vicon_mock":
+        subscription_topics = (
+            "/vicon_mock/CrossSection_0_0/CrossSection_0_0",
+            "/vicon_mock/CrossSection_0_1/CrossSection_0_1",
+            "/vicon_mock/CrossSection_0_2/CrossSection_0_2",
+            "/vicon_mock/CrossSection_0_3/CrossSection_0_3",
+            # "/vicon_mock/CrossSection_0_4/CrossSection_0_4",
+            # "/vicon_mock/CrossSection_0_5/CrossSection_0_5",
+        )
+    return subscription_topics
 
 
 @click.command()
 @click.option(
     "--log-level",
     type=click.Choice(
-        ["debug", "info", "warn", "error", "fatal"], case_sensitive=False
+        ["debug", "info", "warn", "error", "fatal"],
+        case_sensitive=False,
     ),
     default="info",
     help="Set the logging level",
 )
-def main(log_level):
+@click.option(
+    "--source",
+    type=click.Choice(
+        ["vicon", "vicon_mock"],
+        case_sensitive=False,
+    ),
+    default="vicon",
+    help="Set the source of the listener",
+)
+def main(log_level: str, source: str):
     rclpy.init()
-    subscription_topics = (
-        "/vicon/br2_seg_1/br2_seg_1",
-        "/vicon/br2_seg_2/br2_seg_2",
-        "/vicon/br2_seg_3/br2_seg_3",
-        "/vicon/br2_seg_4/br2_seg_4",
-    )
-    # subscription_topics = (
-    #     "/vicon_mock/CrossSection_0_0/CrossSection_0_0",
-    #     "/vicon_mock/CrossSection_0_1/CrossSection_0_1",
-    #     '/vicon_mock/CrossSection_0_2/CrossSection_0_2',
-    #     '/vicon_mock/CrossSection_0_3/CrossSection_0_3',
-    #     # '/vicon_mock/CrossSection_0_4/CrossSection_0_4',
-    #     # '/vicon_mock/CrossSection_0_5/CrossSection_0_5',
-    # )
     node = ReconstructionNode(
-        subscription_topics=subscription_topics,
+        subscription_topics=set_subsciption_topics(source),
         log_level=log_level,
     )
     try: