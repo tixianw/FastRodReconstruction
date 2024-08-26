9d006cd63f2005e597022623ff810e36116af1b3
tixianw <tixianw@gmail.com>
2024-08-25 21:33:12 -0500

    add example files for 3D_full_reconstruction

---
+++ examples/run_PCA_octopus.py
@@ -0,0 +1,105 @@
+"""
+Created on Aug 25, 2024
+@author: Tixian Wang
+"""
+
+from importlib import resources
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from assets import ASSETS
+from assets import FILE2_NAME as FILE_NAME
+from neural_data_smoothing3D_full import PCA, pos_dir_to_input
+from neural_data_smoothing3D_full.utils import _aver
+
+color = ["C" + str(i) for i in range(10)]
+
+
+def main():
+    with resources.path(ASSETS, FILE_NAME) as path:
+        print('Reading file', FILE_NAME, '...')
+        data = np.load(path, allow_pickle="TRUE").item()
+    
+    n_elem = data["model"]["n_elem"]
+    L = data["model"]["L"]
+    radius = data["model"]["radius"]
+    s = data["model"]["s"]
+    s_mean = _aver(s)
+    dl = data["model"]["dl"]
+    nominal_shear = data["model"]["nominal_shear"]
+    
+    ## data point setup
+    n_data_pts = 3  # 5 # exlude the initial point at base
+    idx_data_pts = np.array(
+        [int(100 / (n_data_pts)) * i for i in range(1, n_data_pts)] + [-1]
+    )
+    print("idx of s_j's:", idx_data_pts)
+
+    position = data["true_pos"]
+    director = data["true_dir"]
+
+    input_pos = position[..., idx_data_pts]
+    input_dir = director[..., idx_data_pts]
+    input_data = pos_dir_to_input(input_pos, input_dir)
+
+    # true_pos = data['true_pos']
+    # true_dir = data['true_dir']
+    true_kappa = data["true_kappa"]
+    true_shear = data['true_shear']
+
+    n_components = np.array([3, 3, 3, 3, 3, 3])
+    pca_list = []
+    for i in range(len(n_components)):
+        pca = PCA(n_components=n_components[i])
+        if i < 3:
+            pca.fit(true_kappa[:, i, :])
+        else:
+            pca.fit(true_shear[:, i%3, :])
+        pca_list.append(pca)
+
+    flag_save = 0
+
+    if flag_save:
+        import os
+
+        folder_name = "assets"
+        if not os.path.exists(folder_name):
+            os.mkdir(folder_name)
+
+        print("saving data to file", FILE_NAME, '...')
+
+        data["n_data_pts"] = n_data_pts
+        data["idx_data_pts"] = idx_data_pts
+        data["input_data"] = input_data
+        data["pca"] = pca_list
+        np.save(folder_name + '/' + FILE_NAME, data)
+
+    # np.random.seed(2024)
+    # idx_list = np.random.randint(len(true_kappa), size=10) # [i*10 for i in range(10)]
+    # pos_approximate = curvature2position(kappa_approximate)
+    # for ii in range(len(idx_list)):
+    # 	i = idx_list[ii]
+    # 	plt.figure(1)
+    # 	plt.plot(s[1:-1], true_kappa[i,:], color=color[ii], ls='-')
+    # 	plt.plot(s[1:-1], kappa_approximate[i,:], color=color[ii], ls='--')
+    # 	plt.figure(2)
+    # 	plt.plot(pos_approximate[i,0,:], pos_approximate[i,1,:], color=color[ii], ls='-')
+    # 	plt.plot(true_pos[i,0,:], true_pos[i,1,:], color=color[ii], ls='--')
+    # 	plt.scatter(input_data[i,0,:], input_data[i,1,:], s=50, marker='o', color=color[ii])
+
+    for i in range(len(n_components)):
+        print('strain', i, ': mean =', pca_list[i].mean.mean(), ', std =', pca_list[i].std.std())
+        plt.figure(i)
+        for j in range(n_components[i]):
+            if i < 3:
+                plt.plot(s[1:-1], pca_list[i].components[:, j])
+            else:
+                plt.plot(s_mean, pca_list[i].components[:, j])
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
+    # main(sys.argv[1])
---
+++ examples/run_training_octopus.py
@@ -0,0 +1,115 @@
+"""
+Created on Aug 21, 2024
+@author: Tixian Wang
+"""
+
+import os
+from importlib import resources
+
+import numpy as np
+
+from assets import ASSETS, FILE_NAME
+from neural_data_smoothing3D import CurvatureSmoothing3DModel, TensorConstants
+from neural_data_smoothing3D.utils import _aver
+
+# torch.manual_seed(2024)
+# npr.seed(2024)
+
+
+def main():
+
+    folder_name = "assets"
+    training_data_name = "training_data_set_br2.npy"
+    if not os.path.exists(folder_name):
+        raise FileNotFoundError("Run create_training_set.py first")
+
+    # Construct the full path to the file
+    file_path = os.path.join(folder_name, training_data_name)
+
+    # Check if the file exists
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(
+            f"The file '{training_data_name}' does not exist in the '{folder_name}' folder. "
+            f"Run create_training_set.py first"
+        )
+
+    with resources.path(ASSETS, FILE_NAME) as path:
+        data = np.load(path, allow_pickle="TRUE").item()
+
+    n_elem = data["model"]["n_elem"]
+    L = data["model"]["L"]
+    radius = data["model"]["radius"]
+    s = data["model"]["s"]
+    dl = data["model"]["dl"]
+    nominal_shear = data["model"]["nominal_shear"]
+    idx_data_pts = data["idx_data_pts"]
+    input_data = data["input_data"]
+    true_pos = data["true_pos"]
+    true_dir = data["true_dir"]
+    true_kappa = data["true_kappa"]
+    pca = data["pca"]
+
+    training_data = np.load(
+        file_path, allow_pickle="TRUE"
+    ).item()
+    input_data = training_data["input_data"]
+    true_pos = training_data["true_pos"]
+    true_dir = training_data["true_dir"]
+    true_kappa = training_data["true_kappa"]
+    # nominal_shear = training_data['true_shear']
+    input_size = training_data["input_size"]
+    output_size = training_data["output_size"]
+
+    E = 10**6
+    G = E * 2 / 3
+    A = np.pi * (radius.mean(axis=0)) ** 2
+    bend_twist_stiff = ((_aver(A)) ** 2 / (4 * np.pi))[None, None, :] * np.diag(
+        [E, E, 2 * G]
+    )[..., None]
+
+    power_chi_r = 5  # 6 # 5 # 4 # 3
+    power_chi_d = 5
+    chi_r = 10**power_chi_r  # 1
+    chi_d = 10**power_chi_d
+    chi_u = 0  # 1e-5
+
+    tensor_constants = TensorConstants(
+        bend_twist_stiff,
+        idx_data_pts,
+        dl,
+        chi_r,
+        chi_d,
+        pca,
+        input_size,
+        output_size,
+    )
+    ## Train the model
+    num_epochs = int(10)
+    batch_size = 128  # 128 # 100
+    print(
+        "# total samples:",
+        len(input_data),
+        "# epochs:",
+        num_epochs,
+        "batch size:",
+        batch_size,
+    )
+    model = CurvatureSmoothing3DModel(
+        tensor_constants,
+        input_data,
+        num_epochs,
+        batch_size=batch_size,
+        labels=true_kappa,
+    )
+
+    model.model_train()
+
+    flag_save = True
+    model_name = "/data_smoothing_model_br2_test.pt"
+
+    if flag_save:
+        model.model_save(folder_name + model_name)
+
+
+if __name__ == "__main__":
+    main()
