1caede41a72de98739110dfce6c73ad8d6d90519
Hanson-HSChang <hanson.hschang@gmail.com>
2024-09-05 19:13:00 -0500

    wip: update assets files

--- src/assets/__init__.py
+++ src/assets/__init__.py
@@ -1,8 +1,14 @@
 ASSETS = "assets"
 
-FILE_NAME_BR2 = "BR2_arm_data.npy"
-MODEL_NAME_BR2 = "data_smoothing_model_br2_3markers.pt"
+FILE_NAME_BR2 = {
+    3: "BR2_arm_data.npy",
+    4: "BR2_arm_data_4markers.npy",
+}
+MODEL_NAME_BR2 = {
+    3: "data_smoothing_model_br2_3markers.pt",
+    4: "data_smoothing_model_br2_4markers.pt",
+}
 
 FILE_NAME_OCTOPUS = "octopus_arm_data.npy"
-FILE_NAME_OCTOPUS_H5 = 'octopus_arm_data.h5'
+FILE_NAME_OCTOPUS_H5 = "octopus_arm_data.h5"
 MODEL_NAME_OCTOPUS = "data_smoothing_model_octopus.pt"
--- src/reconstruction/model.py
+++ src/reconstruction/model.py
@@ -32,31 +32,41 @@ class ReconstructionModel:
         self,
         data_file_name: Optional[str] = None,
         model_file_name: Optional[str] = None,
-        number_of_markers: int = 3,
+        number_of_markers: int = 4,
         base_pose: np.ndarray = np.diag([1, -1, -1, 1]),
     ):
 
+        self.number_of_markers = number_of_markers
         if data_file_name:
             self.data_file_name = data_file_name
             rod_data = np.load(self.data_file_name, allow_pickle="TRUE").item()
         else:
-            with resources.path(ASSETS, FILE_NAME_BR2) as path:
+            with resources.path(
+                ASSETS, FILE_NAME_BR2[self.number_of_markers]
+            ) as path:
                 rod_data = np.load(path, allow_pickle="TRUE").item()
             self.data_file_name = (
-                "package_assets:" + ASSETS + "/" + FILE_NAME_BR2
+                "package_assets:"
+                + ASSETS
+                + "/"
+                + FILE_NAME_BR2[self.number_of_markers]
             )
 
         if model_file_name:
             self.model_file_name = model_file_name
             model_data = torch.load(self.model_file_name)
         else:
-            with resources.path(ASSETS, MODEL_NAME_BR2) as path:
+            with resources.path(
+                ASSETS, MODEL_NAME_BR2[self.number_of_markers]
+            ) as path:
                 model_data = torch.load(path)
             self.model_file_name = (
-                "package_assets:" + ASSETS + "/" + MODEL_NAME_BR2
+                "package_assets:"
+                + ASSETS
+                + "/"
+                + MODEL_NAME_BR2[self.number_of_markers]
             )
 
-        self.number_of_markers = number_of_markers
         self.calibrated_pose = np.zeros((4, 4, self.number_of_markers))
         self.input_pose = np.repeat(
             np.expand_dims(np.diag([1.0, -1.0, -1.0, 1.0]), axis=2),
