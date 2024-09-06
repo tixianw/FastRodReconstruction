47d9433b5d89ff0d8a8461a67921c12694b29701
tixianw <tixianw@gmail.com>
2024-09-05 19:19:55 -0500

    cleaned src/assets and add new trained model with Gaussian no-noise and potential energy loss added

--- src/assets/__init__.py
+++ src/assets/__init__.py
@@ -1,7 +1,7 @@
 ASSETS = "assets"
 
 FILE_NAME_BR2 = "BR2_arm_data.npy"
-MODEL_NAME_BR2 = "data_smoothing_model_br2_3markers.pt"
+MODEL_NAME_BR2 = "data_smoothing_model_br2_4markers.pt"
 
 FILE_NAME_OCTOPUS = "octopus_arm_data.npy"
 FILE_NAME_OCTOPUS_H5 = 'octopus_arm_data.h5'
