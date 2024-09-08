00635afd1f925662f139a29dcf6971fa0b440616
tixianw <tixianw@gmail.com>
2024-09-08 10:51:31 -0500

    run reconstruction on samples for time cost and test loss statistics

--- examples/octopus/model_eval_octopus.py
+++ examples/octopus/model_eval_octopus.py
@@ -9,16 +9,16 @@ from importlib import resources
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
-# from time import perf_counter
+from time import perf_counter
 
 from assets import ASSETS
 from file_global import FILE_NAME, MODEL_NAME
 from neural_data_smoothing3D_full import (
-    CurvatureSmoothing3DNet,
-    CurvatureSmoothing3DLoss,
-    coeff2strain,
-    strain2posdir,
-    tensor2numpyVec,
+	CurvatureSmoothing3DNet,
+	CurvatureSmoothing3DLoss,
+	coeff2strain,
+	strain2posdir,
+	tensor2numpyVec,
 )
 from neural_data_smoothing3D_full.utils import _aver
 
@@ -26,30 +26,30 @@ color = ["C" + str(i) for i in range(10)]
 
 ## import rod parameters
 with resources.path(ASSETS, FILE_NAME) as path:
-    data = np.load(path, allow_pickle="TRUE").item()
+	data = np.load(path, allow_pickle="TRUE").item()
 
 user_data_flag = True # False # 
 
 if user_data_flag:
-    folder_name = "assets" # 'Data' # 
-    test_data_name = "training_data_set_octopus_noisy_4basis.npy"
-    # model_name = 'data_smoothing_model_octopus_test.pt'
-    model_name = 'data_smoothing_model_octopus_test_4basis' # noise2'
-    idx = 100
-    model_name += '_epoch%03d'%(idx) + '.pt'
-    model_file_path = os.path.join(folder_name, model_name)
-    test_data_file_path = os.path.join(folder_name, test_data_name)
-    model = torch.load(model_file_path)
-    test_data = data
-    # test_data = np.load(
-    #     test_data_file_path, allow_pickle="TRUE"
-    # ).item()
-    print('Evalulating user\'s trained model...')
+	folder_name = "assets" # 'Data' # 
+	test_data_name = "training_data_set_octopus_noisy_4basis.npy"
+	# model_name = 'data_smoothing_model_octopus_test.pt'
+	model_name = 'data_smoothing_model_octopus_test_4basis' # noise2'
+	idx = 100
+	model_name += '_epoch%03d'%(idx) + '.pt'
+	model_file_path = os.path.join(folder_name, model_name)
+	test_data_file_path = os.path.join(folder_name, test_data_name)
+	model = torch.load(model_file_path)
+	test_data = data
+	# test_data = np.load(
+	#     test_data_file_path, allow_pickle="TRUE"
+	# ).item()
+	print('Evalulating user\'s trained model...')
 else:
-    with resources.path(ASSETS, MODEL_NAME) as path:
-        model = torch.load(path)
-    test_data = data
-    print('No user trained model. Evalulating developer\'s trained model...')
+	with resources.path(ASSETS, MODEL_NAME) as path:
+		model = torch.load(path)
+	test_data = data
+	print('No user trained model. Evalulating developer\'s trained model...')
 
 ## import rod parameters
 n_elem = data["model"]["n_elem"]
@@ -84,21 +84,21 @@ losses, vlosses, test_loss = model["losses"]
 n_iter = int(len(losses) / len(vlosses))
 
 print(
-    "# epochs:",
-    num_epochs,
-    "batch size:",
-    batch_size,
-    "regularizations: chi_r=",
-    tensor_constants.chi_r,
-    ', chi_d=',
-    tensor_constants.chi_d,
+	"# epochs:",
+	num_epochs,
+	"batch size:",
+	batch_size,
+	"regularizations: chi_r=",
+	tensor_constants.chi_r,
+	', chi_d=',
+	tensor_constants.chi_d,
 )
 print("min_loss:", min(losses))
 
 plt.figure(0)
 plt.semilogy(np.arange(len(losses))/n_iter, losses, ls="-", label="train")
 plt.semilogy(
-    np.arange(len(vlosses)), vlosses, ls="--", label="validation"
+	np.arange(len(vlosses)), vlosses, ls="--", label="validation"
 )
 plt.scatter(len(vlosses), test_loss, s=50, marker="o", color="C3", label="test")
 plt.xlabel("epochs") #  * iterations
@@ -109,19 +109,28 @@ plt.legend()
 input_tensor = torch.from_numpy(input_data).float()
 np.random.seed() # 2024
 idx_list = np.random.randint(
-    len(input_data), size=10
+	len(input_data), size=10
 )  # [i*10 for i in range(6)]
 
-# net.eval()
-# print('start counting...')
-# start = perf_counter()
-# for i in range(len(input_data)):
-# 	output = net(input_tensor[i])
-# 	strain_output = coeff2strain(tensor2numpyVec(output), pca)
-# 	[position_output, director_output] = strain2posdir(strain_output, dl)
-# stop = perf_counter()
-# print((stop-start), 1e5/(stop-start))
-# quit()
+net.eval()
+print('start counting...')
+test_loss = []
+time_cost = []
+start0 = perf_counter()
+print('total number of samples:', len(input_data))
+for i in range(len(input_data)):
+	start = perf_counter()
+	output = net(input_tensor[i])
+	strain_output = coeff2strain(tensor2numpyVec(output), pca)
+	[position_output, director_output] = strain2posdir(strain_output, dl, true_dir[0,...,0])
+	stop = perf_counter()
+	time_cost.append(stop - start)
+	t_loss = loss_fn(output, input_tensor[i][None,:,:])
+	test_loss.append(t_loss)
+stop0 = perf_counter()
+print('total computational time:', (stop0-start0), 'averaged freqency:', len(input_data)/(stop0-start0))
+statistics = {'n_sample': len(input_data), 'time_cost': time_cost, 'test_loss': test_loss}
+quit()
 
 net.eval()
 test_loss = []
@@ -129,49 +138,49 @@ fig = plt.figure(1)
 ax = fig.add_subplot(111, projection="3d")
 fig2, axes = plt.subplots(ncols=3, nrows=2, sharex=True, figsize=(16, 5))
 for ii in range(len(idx_list)):
-    i = idx_list[ii]
-    with torch.no_grad():
-        output = net(input_tensor[i])
-        t_loss = loss_fn(output, input_tensor[i][None,:,:])
-        test_loss.append(t_loss)
-    strain_output = coeff2strain(tensor2numpyVec(output), pca)
-    [position_output, director_output] = strain2posdir(strain_output, dl, true_dir[0,...,0])
-    ax.plot(
-        position_output[0, 0, :],
-        position_output[0, 1, :],
-        position_output[0, 2, :],
-        color=color[ii],
-        ls="-",
-    )
-    ax.plot(
-        true_pos[i, 0, :],
-        true_pos[i, 1, :],
-        true_pos[i, 2, :],
-        color=color[ii],
-        ls="--",
-    )
-    ax.scatter(
-        input_data[i, 0, :],
-        input_data[i, 1, :],
-        input_data[i, 2, :],
-        s=50,
-        marker="o",
-        color=color[ii],
-    )
-    ax.set_xlim(0, L)
-    ax.set_ylim(0, L)
-    ax.set_zlim(0, L)
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('z')
-    ax.set_aspect("equal")
-    for j in range(3):
-        axes[0][j].plot(s[1:-1], strain_output[0][0, j, :], color=color[ii], ls="-")
-        axes[0][j].plot(s[1:-1], true_kappa[i, j, :], color=color[ii], ls="--")
-        axes[1][j].plot(s_mean, strain_output[1][0, j, :], color=color[ii], ls="-")
-        axes[1][j].plot(s_mean, true_shear[i, j, :], color=color[ii], ls="--")
-        axes[0][j].set_ylabel('$\\kappa_%d$'%(j+1))
-        axes[1][j].set_ylabel('$\\nu_%d$'%(j+1))
+	i = idx_list[ii]
+	with torch.no_grad():
+		output = net(input_tensor[i])
+		t_loss = loss_fn(output, input_tensor[i][None,:,:])
+		test_loss.append(t_loss)
+	strain_output = coeff2strain(tensor2numpyVec(output), pca)
+	[position_output, director_output] = strain2posdir(strain_output, dl, true_dir[0,...,0])
+	ax.plot(
+		position_output[0, 0, :],
+		position_output[0, 1, :],
+		position_output[0, 2, :],
+		color=color[ii],
+		ls="-",
+	)
+	ax.plot(
+		true_pos[i, 0, :],
+		true_pos[i, 1, :],
+		true_pos[i, 2, :],
+		color=color[ii],
+		ls="--",
+	)
+	ax.scatter(
+		input_data[i, 0, :],
+		input_data[i, 1, :],
+		input_data[i, 2, :],
+		s=50,
+		marker="o",
+		color=color[ii],
+	)
+	ax.set_xlim(0, L)
+	ax.set_ylim(0, L)
+	ax.set_zlim(0, L)
+	ax.set_xlabel('x')
+	ax.set_ylabel('y')
+	ax.set_zlabel('z')
+	ax.set_aspect("equal")
+	for j in range(3):
+		axes[0][j].plot(s[1:-1], strain_output[0][0, j, :], color=color[ii], ls="-")
+		axes[0][j].plot(s[1:-1], true_kappa[i, j, :], color=color[ii], ls="--")
+		axes[1][j].plot(s_mean, strain_output[1][0, j, :], color=color[ii], ls="-")
+		axes[1][j].plot(s_mean, true_shear[i, j, :], color=color[ii], ls="--")
+		axes[0][j].set_ylabel('$\\kappa_%d$'%(j+1))
+		axes[1][j].set_ylabel('$\\nu_%d$'%(j+1))
 
 
 test_loss = np.array(test_loss)
